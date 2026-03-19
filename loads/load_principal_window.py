3242c2465792b65b35b254b8700c41983664a688
Justin <justinfranco.a12@gmail.com>
2026-03-19 12:44:43 -0600

    1.4 funcional

--- loads/load_principal_window.py
+++ loads/load_principal_window.py
@@ -8,6 +8,8 @@ class LoadPrincipal ( QtWidgets.QMainWindow ) :
         uic.loadUi( "gui/main_window.ui"  , self )
         self.show() 
 
+        self.actionRegresion.triggered.connect( self.regresion )
+        self.actionIntegracion.triggered.connect( self.integracion )
         self.btnIntegracion.clicked.connect( self.regresion )
         self.btnRegresion.clicked.connect( self.integracion )
         
